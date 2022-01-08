4ae054dca2e1ce9682bc1dccebcb7d654c6e339a
juniotavares <juniotavares@outlook.com.br>
2022-01-08 18:40:08 -0300

    estrutura de um site

---
+++ Introdução ao javascript/assets/funcoes.js
@@ -0,0 +1,46 @@
+/*var currentNumerWapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function max(){
+  
+}
+
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumerWapper.innerHTML= currentNumber;
+
+    if (currentNumber >= 10) {
+        currentNumber = null;
+    } else {
+        console.log("Continue clicando")
+    }
+
+
+
+}
+
+function decrement(){
+    currentNumber = currentNumber - 1;
+    currentNumerWapper.innerHTML= currentNumber;
+
+   
+}
+*/
+
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
--- Introdução ao javascript/comentarios.js
+++
@@ -1,6 +0,0 @@
-// Esse equivale ao comentario linha 
-/* esse equivale a um cometario 
-de varias linhas */
-
-
-
