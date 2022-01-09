b0d2512cd884ea6956dfa1f699bf501988fea993
juniotavares <juniotavares@outlook.com.br>
2022-01-08 22:03:56 -0300

    Introdução ao JavaScript

--- Introdução ao javascript/assets/funcoes.js
+++ Introdução ao javascript/assets/funcoes.js
@@ -34,11 +34,14 @@ const CURRENT_NUMBER = document.getElementById('currentNumber');
 function increment() {
 	count++;
 	CURRENT_NUMBER.innerHTML = count;
+    
 }
 
 function decrement() {
 	count--;
 	CURRENT_NUMBER.innerHTML = count;
+
+   
 }
 
 function test() {
