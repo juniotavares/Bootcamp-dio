4d6bb01325bc79a62a7f1a5ef7cde8ec57032ec0
juniotavares <juniotavares@outlook.com.br>
2022-01-11 20:55:11 -0300

    Estruturas condicionais

---
+++ SintaxeBasicaEmJavaScript/pratc/aula2/script.js
@@ -0,0 +1,31 @@
+// let array = ['String', 1, true]
+// console.log(array)
+
+let array =['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+console.log(array[5])
+// forEach 
+
+// array.forEach(function(item, index) { console.log(item, index)
+    
+// });
+
+// array.push([]);
+// console.log(array);
+
+// array.pop();
+// console.log(array);
+
+// array.shift();
+// console.log(array);
+
+// array.unshift("novo item no inicio");
+// console.log(array);
+
+// console.log(array.indexOf(true));
+
+// array.splice(0 ,3);
+// console.log(array);
+
+// let novoarray = array.slice(0, 3);
+// console.log(novoarray)
+
